687090e342d56354be993e39d04b7d992ce2ac64
PC <1377318286@qq.com>
2017-06-08 21:05:37 +0800

    Add 207

---
+++ 207_CourseSchedule/207_CourseSchedule.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+
+	}
+};
+
+int main(int argc, char *argv[]){
+	Solution s;
+	cout << s.canFinish(2, vector<pair<int,int>>{{1, 0}, {0, 1}}) << endl;
+	system("pause");
+	return 0;
+}
