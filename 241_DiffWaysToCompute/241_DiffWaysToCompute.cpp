3a8aae9351da7909172244228b3d61ede72bf693
PC <1377318286@qq.com>
2017-06-21 09:02:41 +0800

    Divide and Conquer

---
+++ 241_DiffWaysToCompute/241_DiffWaysToCompute.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<int> diffWaysToCompute(string input) {
+		vector<int> ret;
+		for (int i = 0; i < input.size(); ++i){
+			if (input[i] == '+' || input[i] == '-' || input[i] == '*'){
+				vector<int> left = diffWaysToCompute(input.substr(0,i));
+				vector<int> right = diffWaysToCompute(input.substr(i+1));
+				for (int j = 0; j < left.size(); ++j){
+					for (int k = 0; k < right.size(); ++k){
+						if (input[i] == '+'){
+							ret.push_back(left[j] + right[k]);
+						} else if (input[i] == '-'){
+							ret.push_back(left[j] - right[k]);
+						} else{
+							ret.push_back(left[j] * right[k]);
+						}
+					}
+				}
+			}
+		}
+		if (ret.empty()){
+			ret.push_back(atoi(input.c_str()));
+		}
+		return ret;
+	}
+};
+
+int main(int argc, char *argv[]){
+	Solution s;
+	auto rv = s.diffWaysToCompute("2*3-4*5");
+	for (auto v : rv){
+		cout << v << ", ";
+	}
+	system("pause");
+	return 0;
+}
