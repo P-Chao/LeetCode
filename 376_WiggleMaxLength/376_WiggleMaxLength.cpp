a00022f15265f5047e94f9602b9a1ff86bec9be6
PC <1377318286@qq.com>
2017-06-27 16:58:59 +0800

    Submit Solution

--- 376_WiggleMaxLength/376_WiggleMaxLength.cpp
+++ 376_WiggleMaxLength/376_WiggleMaxLength.cpp
@@ -6,13 +6,39 @@ using namespace std;
 class Solution {
 public:
 	int wiggleMaxLength(vector<int>& nums) {
-
+		const int n = nums.size();
+		if (n < 2){
+			return n;
+		}
+		int res = 1, prev = nums[0], i = 1;
+		while (i < n && nums[i] == nums[i-1]){
+			++i;
+		}
+		if (i == n){
+			return res;
+		}
+		bool inc = (nums[i] > nums[i-1]);
+		++i; ++res;
+		for (; i < n; ++i){
+			if (nums[i] > nums[i - 1]){
+				if (!inc){
+					res++;
+					inc = true;
+				}
+			} else if (nums[i] < nums[i - 1]){
+				if (inc){
+					res++;
+					inc = false;
+				}
+			}
+		}
+		return res;
 	}
 };
 
 int main(int argc, char *argv[]){
 	Solution s;
-	cout << s.wiggleMaxLength(vector<int>{1, 17, 5, 10, 13, 15, 10, 5, 16, 8}) << endl;
+	cout << s.wiggleMaxLength(vector<int>{1, 2, 3, 4, 5, 6, 7, 8, 9}) << endl;
 	system("pause");
 	return 0;
 }
---
+++ 377_CombinationSumIV/377_CombinationSumIV.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int combinationSum4(vector<int>& nums, int target) {
+
+	}
+};
+
+int main(int argc, char *argv[]){
+	Solution s;
+	cout << s.combinationSum4(vector<int>{1, 2, 3}, 4) << endl;
+	system("pause");
+	return 0;
+}
