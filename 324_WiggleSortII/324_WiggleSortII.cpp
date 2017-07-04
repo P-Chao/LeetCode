79a53d2cf447a6cef886099e3a6af5c39d34db66
PC <1377318286@qq.com>
2017-07-04 09:00:51 +0800

    Wiggle Sort

--- 324_WiggleSortII/324_WiggleSortII.cpp
+++ 324_WiggleSortII/324_WiggleSortII.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
 	void wiggleSort(vector<int>& nums) {
-		const int n = nums.size();
+		vector<int> sorted(nums);
+		sort(sorted.begin(), sorted.end());
+		for (int i = nums.size() - 1, j = 0, k = i / 2 + 1; i >= 0; --i){
+			nums[i] = sorted[i & 1 ? k++ : j++];
+		}
+		return;
 	}
 };
 
