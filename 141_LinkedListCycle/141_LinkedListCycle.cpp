f9712fdc8bee0616f03db7758e917000d262c773
PC <1377318286@qq.com>
2017-07-17 08:30:06 +0800

    Detect Linkedlist Cycle

---
+++ 141_LinkedListCycle/141_LinkedListCycle.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	bool hasCycle(ListNode *head) {
+		if (!head) return false;
+		ListNode *fast = head, *slow = head;
+		while (fast && fast->next){
+			fast = fast->next->next;
+			slow = slow->next;
+			if (fast == slow){
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+int main(int argc, char *argv[]){
+	Solution s;
+	cout << s.hasCycle(NULL) << endl;
+	system("pause");
+	return 0;
+}
--- Test/Test.cpp
+++ Test/Test.cpp
@@ -5,8 +5,92 @@
 #include<unordered_map>
 #include<algorithm>
 #include<queue>
+#include<sstream>
 
 using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+void ReplaceAll(std::string& strSource, const std::string& strOld, const std::string& strNew)
+{
+	int nPos = 0;
+	while ((nPos = strSource.find(strOld, nPos)) != strSource.npos)
+	{
+		strSource.replace(nPos, strOld.length(), strNew);
+		nPos += strNew.length();
+	}
+}
+
+class Solution{
+public:
+	string reconstructList(string str){
+		const int n = 0;
+		ReplaceAll(str, string("->"), string(" "));
+		ReplaceAll(str, string("NULL"), string(""));
+		
+		ListNode dummy(0);
+		ListNode* node = &dummy;
+
+		stringstream ss;
+		ss << str;
+		string strtmp;
+		while (ss >> strtmp){
+			if (ss.fail()){
+				break;
+			}
+			int tmp = atoi(strtmp.c_str());
+			node->next = new ListNode(tmp);
+			node = node->next;
+		}
+
+		ListNode dummy0(0);
+		ListNode* node0 = &dummy0;
+		while (node->next && node->next->next) {
+			auto tmp = node->next->next;
+			node0->next = node->next;
+			node->next = tmp;
+		}
+		if (node){
+			node->next = dummy0.next;
+		}
+
+		char buf[50];
+		string res;
+		node = &dummy;
+		while (node->next) {
+			sprintf(buf, "%d", node->next->val);
+			res = res + "->" + string(buf);
+		}
+		res += "->NULL";
+
+		return res.substr(2);
+	}
+
+	int longestSubarray(vector<int> nums){
+		const int n = nums.size();
+		if (n < 2){
+			return 0;
+		}
+		int ans = 0;
+		int prev = 0;
+		unordered_map<int, int> dp;
+		dp.insert({0,-1});
+		for (int i = 0; i < n; ++i){
+			int tmp = nums[i] ? 1 : -1;
+			prev += tmp;
+			if (dp.find(prev) != dp.end()){
+				ans = max(ans, i-dp[prev]);
+			} else{
+				dp[prev] = i;
+			}
+		}
+		return ans;
+	}
+};
 
 class Tran{
 public:
@@ -91,10 +175,9 @@ public:
 };
 
 int main(int argc, char *argv[]){
-	Tran tr;
-	Cmp c;
-	// cout << tr.tran(8, 16, string("12345670")) << endl;
-	cout << c.cmp(1, 4, vector<vector<int>>{ {3, 1}, { 3, 4 }, { 1, 1 } }, 3) << endl;
+	Solution s;
+	cout << s.reconstructList("1->2->3->4->5->NULL") << endl;
+	//cout << s.longestSubarray(vector<int>{0, 0, 0, 1, 0, 1, 1, 1, 1}) << endl;
 	system("pause");
 	return 0;
 }
